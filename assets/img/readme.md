b57f83a9503157f8cd7fa759d71516faed4f715c
Allyson Eduardo dos Santos Silva <allysoneduardo04@gmail.com>
2021-10-12 14:34:43 -0300

    update

--- assets/img/readme.md
+++
@@ -1,1 +0,0 @@
-# Here will be downloaded your wallpapers, if you notice it downloading to other folder, let me know at repo. [https://github.com/Allyson04/wallpaper-scraping](https://github.com/Allyson04/wallpaper-scraping)
--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import requests
 import shutil
 
 currentDirectory = os.getcwd()
-WebDriver  = webdriver.Chrome(f"{currentDirectory}//assets//driver//chromedriver.exe")
+WebDriver  = webdriver.Chrome(f"{currentDirectory}//wallpaper-scraping//assets//driver//chromedriver.exe")
 wait = WebDriverWait(WebDriver, 10)
 WebDriver.get("https://wallhaven.cc/")
 WebDriver.find_element_by_class_name("random").click()
@@ -16,16 +16,24 @@ mainPage = WebDriver.window_handles[0]
 try:
     cycles = str(sys.argv[1])
 except:
-    cycles = 100
+    cycles = 10000
 finally:
-    for n in range(cycles):
-        WebDriver.find_element_by_css_selector(f'li:nth-child({n+1}) figure').click()
-        wallpaperPage = WebDriver.window_handles[1]
-        WebDriver.switch_to.window(wallpaperPage)
-        wallpaper = WebDriver.find_element_by_css_selector('#wallpaper').get_attribute("src")
-        response = requests.get(wallpaper, stream=True)
-        with open(f"{currentDirectory}//assets//img//{wallpaper[-10:-4]}.png", "wb") as out_file:
-            shutil.copyfileobj(response.raw, out_file)
-        del response
-        WebDriver.close()
-        WebDriver.switch_to.window(mainPage)
+    rangeOfImages = 8
+    refreshes = int(cycles/rangeOfImages)
+    print(refreshes)
+    for x in range(refreshes):
+        for n in range(rangeOfImages):
+            # print(n)
+            WebDriver.find_element_by_css_selector(f'li:nth-child({n+1}) figure').click()
+            wallpaperPage = WebDriver.window_handles[1]
+            WebDriver.switch_to.window(wallpaperPage)
+            wallpaper = WebDriver.find_element_by_css_selector('#wallpaper').get_attribute("src")
+            response = requests.get(wallpaper, stream=True)
+            with open(f"{currentDirectory}//wallpaper-scraping//assets//img//{wallpaper[-10:-4]}.png", "wb") as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
+            WebDriver.close()
+            WebDriver.switch_to.window(mainPage)
+        
+        WebDriver.refresh()
+        n = 0
